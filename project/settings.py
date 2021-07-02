6d5c884155ea10b57ac086186930bdfa6bbc2a9d
shimaa3434 <36128295+shimaa3434@users.noreply.github.com>
2021-07-02 11:11:17 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)()3(5a8h@yuza2(@!^3mt7!)a8x42pclkmv*lf+b39c0iae-*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1:8000', 'crypto-news-2021.herokuapp.com', "*"]
 
